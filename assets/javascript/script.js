6fa658f3c0c6ebb3771bbd8ef19278239f84c9a6
MRusinova <rusinovamihaela@gmail.com>
2022-10-20 07:38:54 +0000

    Add in questions for quiz in script.js

---
+++ assets/javascript/script.js
@@ -0,0 +1,103 @@
+let myQuestions = [
+    {
+        question: "Which type of beans are used to make baked beans?",
+        answers: {
+            a: 'Borlotti beans',
+            b: 'Haricot beans',
+            c: 'Cannellini beans',
+            d: 'Flageolet beans'
+            },
+            correctAnswer: 'b'
+    },
+    {
+        question: "What does IPA stand for?",
+        answers: {
+            a: 'Intensely potent ale',
+            b: 'India pale ale',
+            c: 'India premium ale',
+            d: 'International porter association'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Bobotie is the national dish of which country?",
+        answers: {
+            a: 'Nigeria',
+            b: 'Ethiopia',
+            c: 'South Africa',
+            d: 'Kenya'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Which year did The Great British Bake Off first appear on TV?",
+        answers: {
+            a: '2009',
+            b: '2010',
+            c: '2011',
+            d: '2012'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "If you ordered nigiri from a Japanese restaurant menu, what would you be served?",
+        answers: {
+            a: 'A type of dumpling',
+            b: 'A selection of sliced raw fish & seafood',
+            c: 'A type of sushi featuring rice topped with fish or seafood',
+            d: 'A type of sushi featuring raw fish or seafood wrapped in rice and seaweed'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Which of these chocolate bars does not contain nuts in its ingredient list?",
+        answers: {
+            a: 'Snickers',
+            b: 'Double Decker',
+            c: 'Picnic',
+            d: 'Toblerone'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Which flower does the spice saffron come from?",
+        answers: {
+            a: 'Orchid',
+            b: 'Crocus',
+            c: 'Nasturtium',
+            d: 'Poppy'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "A Whitby Bun is flavoured with what?",
+        answers: {
+            a: 'Raspberry',
+            b: 'Coffee',
+            c: 'Cinnamon',
+            d: 'Lemon'
+            },
+            correctAnswer: 'd'
+    },
+    {
+        question: "Gruyère is a cheese from which country?",
+        answers: {
+            a: 'France',
+            b: 'Switzerland',
+            c: 'Austria',
+            d: 'Germany'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "What are the two main ingredients in the dish Cullen Skink?",
+        answers: {
+            a: 'Smoked haddock and parsnips',
+            b: 'Cabbage and potatoes',
+            c: 'Smoked haddock and potatoes',
+            d: 'Smoked salmon and potatoes'
+        },
+        correctAnswer: 'c'
+    }
+
+]
